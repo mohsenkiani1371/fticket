2b18b7746cd20ff4e33c830e3f77f2b716ab5e14
mohsenkiani1371 <mohsenkiani1371@gmail.com>
2019-12-13 21:58:12 +0330

    Enabling date and time validation in the ticket model

--- models/Ticket.php
+++ models/Ticket.php
@@ -16,9 +16,9 @@ class Ticket extends ActiveRecord
         return [
             [['ownerId', 'flyingFrom', 'flyingTo', 'departureDate', 'departureTime', 'adults', 'airline', 'price'], 'required'],
             [['ownerId', 'adults', 'children', 'infants', 'price'], 'integer'],
-// There is some problem in validating date and time, so I'm not performing any validation on them until I find a solution.
-//            ['departureDate', 'date'],
-//            ['departureTime', 'time']
+//			If there's a problem in validating date and time when working on front end, disable following validations.
+           ['departureDate', 'date'],
+           ['departureTime', 'time']
         ];
     }
 }
